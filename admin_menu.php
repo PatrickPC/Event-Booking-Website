b7050da77eda2071aeebcc740cd6d82782703e18
Patrick P.C. <pratikchaudhary270@gmail.com>
2025-07-11 19:02:14 +0545

    Add: Admin Landing Page

---
+++ admin_menu.php
@@ -0,0 +1,54 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cit-E Cycling</title>
+    <link rel="stylesheet" href="style.scss">
+
+    <style>
+        body{
+            background-image:url(image/Actif\ Numérique\ Distribution\ Enveloppe\ Envato.jpeg) ;
+            width: 100%;
+            position: relative;
+            background-repeat: no-repeat;
+            background-size: cover;
+        
+        }
+    </style>
+   
+</head>
+<body>
+
+        <header>
+            <div class="logo">Cit-E </div>
+            <nav>
+                <a href="#">Guidelines</a>
+                <a href="#">Assets</a>
+                <a href="#">Support</a>
+                <a href="logout.php" >Log Out</a>
+            </nav>
+        </header>
+
+        <div class="container">
+            <div class="admin">
+                <h1>Cit-E Cycling Admin portal</h1>
+            </div>
+           
+            
+            <div class="content">
+                <p>
+                    <br><br><a href="search_form.php" class="btn">Search for clubs or participants</a>
+                    <a href="view_participants_edit_delete.php" class="btn">View all participants to either edit or delete</a>       
+                    <a href="register_form.html" class="btn">Register Now!</a>
+                    <a href="logout.php" class="btn">Log Out</a>
+                </p>
+            </div>
+        </div>
+
+</body>
+</html>
